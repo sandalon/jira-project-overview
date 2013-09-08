3c73fd77462f46a3cf09ce228cde8e4f257a4da9
Corey McClelland <corey@wnd.io>
2013-09-08 17:43:00 -0400

    - mongodb

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ var mongo = require('mongoose');
 
 var app = express();
 
+// mongo-connect
+var conf = {
+  db: {
+    db: 'projectdb',
+    host: '192.168.1.111',
+    port: 27017,  // optional, default: 27017
+    username: 'admin', // optional
+    password: 'secret', // optional
+    collection: 'mySessions' // optional, default: sessions
+  },
+  secret: '076ee61d63aa10a125ea872411e433b9'
+};
+
+// stylus
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
@@ -22,6 +36,14 @@ app.use(stylus.middleware(
   }
 ))
 app.use(express.static(__dirname + '/public'))
+app.use(express.cookieParser());
+app.use(express.session({
+    secret: conf.secret,
+    maxAge: new Date(Date.now() + 3600000),
+    store: new MongoStore(conf.db)
+  }));
+
+
 
 app.get('/', function (req, res) {
   res.render('index',
@@ -29,5 +51,11 @@ app.get('/', function (req, res) {
   )
 })
 
-
-app.listen(3000)
+var dbUrl = 'mongodb://localhost/projectdb';
+// dbUrl += conf.db.username+':'+conf.db.password+'@';
+//dbUrl += conf.db.host+':'+conf.db.port;
+//dbUrl += '/' + conf.db.db;
+mongo.connect(dbUrl);
+mongo.connection.on('open', function () {
+  app.listen(3000);
+});
